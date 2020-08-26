765fc30980611bf138307b4c1435f263275a60ef
Benlee07 <68769195+Benlee07@users.noreply.github.com>
2020-08-27 07:05:54 +0800

    Create Level.java

---
+++ src/game/Entity/Level.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game.Entity;
+
+
+public class Level {
+    public int lvl ;
+    private int speed;
+    private int NumEnemy;
+    
+   public Level(int level)
+    {
+    initLevel(level);
+    lvl = 0;
+    }
+    
+    private void initLevel(int lvel)
+    {
+    this.lvl = lvel;
+    }
+    
+    public int IncMove(int direction, int level)
+    {
+    this.lvl = level;
+    return direction + level;
+    }
+    
+    public int IncLevel(int level)
+    {
+    this.lvl = level;
+    level++;
+    return level;
+    }
+    
+    public void IncEnemy(int lvl)
+    {
+    this.NumEnemy = 2 + lvl;
+    }
+    
+    public void resetlvl()
+    {
+    lvl = 1;
+    NumEnemy = 0;
+    speed = 0;
+    }
+    
+}
