0d533facb6dac6b025065ea21415f427303794d3
noxiD30 <dicksonlow2000@hotmail.com>
2020-08-27 11:15:38 +0800

    update

--- src/game/ADT/ArrListWithIteratorInterface.java
+++ src/game/ADT/ArrListWithIteratorInterface.java
@@ -1,7 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * ListInterface.java An interface for the ADT list. Entries in the list have
+ * positions that begin with 1.
+ *
+ * @author Frank M. Carrano
+ * @version 2.0
  */
 package game.ADT;
 
